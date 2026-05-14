50f1872a46fb3087bf6e12f70f6145712d3c8107
hardzoi22 <hardzoi22@gmail.com>
2026-05-14 13:33:10 +0300

    Update ChatMessages.tsx

--- src/components/chat/ChatMessages.tsx
+++ src/components/chat/ChatMessages.tsx
@@ -4,7 +4,7 @@ import { ChatMessage } from '../../types';
 interface ChatMessagesProps {
   messages: ChatMessage[];
   isStrangerTyping: boolean;
-  chatBottomRef: React.RefObject<HTMLDivElement>;
+  chatBottomRef: React.RefObject<HTMLDivElement | null>;   // ← Исправленный тип
 }
 
 export const ChatMessages: React.FC<ChatMessagesProps> = ({
