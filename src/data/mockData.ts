b413d9efb194913d1260c714fc54982d9f58240e
hardzoi22 <hardzoi22@gmail.com>
2026-05-14 13:08:01 +0300

    Add files via upload

---
+++ src/data/mockData.ts
@@ -0,0 +1,21 @@
+import { StrangerPersona, TopicRoom, Gift, Achievement } from '../types';
+
+export const STRANGER_PERSONAS: StrangerPersona[] = [
+  // ... (оставь те же два персонажа, что были раньше)
+];
+
+export const TOPIC_ROOMS: TopicRoom[] = [
+  // ... (все комнаты из предыдущей версии)
+];
+
+export const AVAILABLE_GIFTS: Gift[] = [
+  // ... (все подарки)
+];
+
+export const ACHIEVEMENTS_LIST: Achievement[] = [
+  // ... (все достижения)
+];
+
+export const generateDailyChallenge = (date: Date) => {
+  // ... (функция генерации ежедневного челленджа)
+};
